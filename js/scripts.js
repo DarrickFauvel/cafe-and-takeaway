d63e29d64048bee2f233cc8ca338aeeb3be6a955
Darrick Fauvel <darrick.fauvel@gmail.com>
2021-09-14 12:16:04 -0400

    Add cart, fonts, images, and style

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 import { message } from './messages.js'
 
+$(function () {
+  $('#order__button').on('click', function () {
+    $('#order__modal').modal('show')
+  })
+})
+
 const hideMessage = (e) => {
   e.preventDefault()
   const targetId = e.target.dataset.targetId
@@ -18,3 +24,8 @@ document.getElementById('message-cookies').addEventListener('click', hideMessage
 populateMessage('important', message.important.title, message.important.text)
 populateMessage('promotion', message.promotion.title, message.promotion.text)
 populateMessage('cookies', message.cookies.title, message.cookies.text)
+
+// Hide messages for now
+document.getElementById('message-important').classList.add('d-none')
+document.getElementById('message-promotion').classList.add('d-none')
+document.getElementById('message-cookies').classList.add('d-none')
